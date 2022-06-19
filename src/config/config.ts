a159209531298764c24f61130459a35af36fd549
Nosenti <inno100os@gmail.com>
2022-06-19 12:21:57 +0200

    initial commit

---
+++ src/config/config.ts
@@ -0,0 +1,22 @@
+export const config = {
+  "dev": {
+    "username": "postgres",
+    "password": "nosenti123",
+    "database": "udagramnosentidev",
+    "host": "udagramnosentidev.c0non0vwii64.us-east-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "us-east-1",
+    "aws_profile": "default",
+    "aws_media_bucket": "udagram-nosenti-dev"
+  },
+  "jwt": {
+    "secret": " "
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
